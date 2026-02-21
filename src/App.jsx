63cc16c3ee89c7f604c99ea3495613abde543fef
Tooba-Malik-tech <tooba2888@gmail.com>
2026-02-21 17:07:39 +0500

    Create src/App.jsx file with main React app component

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            <h1>Welcome to the Advanced Landing Page</h1>
+            <p>This is the main app component.</p>
+        </div>
+    );
+};
+
+export default App;
