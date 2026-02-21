93d0de09b49b9443337b18da98ca544ab84dbbf8
Tooba-Malik-tech <tooba2888@gmail.com>
2026-02-21 17:08:39 +0500

    Add Skills component file

---
+++ src/components/Skills.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Skills = () => {
+    return (
+        <div className="skills">
+            <h2>My Skills</h2>
+            <ul>
+                <li>JavaScript</li>
+                <li>React</li>
+                <li>Node.js</li>
+                <li>CSS</li>
+                <li>HTML</li>
+            </ul>
+        </div>
+    );
+};
+
+export default Skills;
